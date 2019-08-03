224e81ce8652dbe115cc6c939f50f5df3e60d383
Luke Persola <lukepersola@gmail.com>
2019-08-02 18:14:29 -0700

    update instance setup

--- model_setup.sh
+++ model_setup.sh
@@ -1,2 +1,8 @@
 sudo apt update
 sudo apt install python3-pip
+git clone https://github.com/Persola/kaggle-IEEE-CIS-Fraud-Detection.git
+cd kaggle-IEEE-CIS-Fraud-Detection/
+mkdir data
+mkdir data/submissions
+pip3 install -r requirements.txt
+# copy in yur filez: `scp -i ~/.ssh/metis_projects.pem ~/Downloads/asasd.zip ubuntu@54.198.129.32:/home/ubuntu/`
